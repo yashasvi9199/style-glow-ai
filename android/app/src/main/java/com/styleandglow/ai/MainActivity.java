c8605b73645c61c510e544b98536679b27d94805
Yash Haldiya <yashasvi9199@gmail.com>
2025-11-28 16:04:49 +0530

    test: added code to allow debug in manifest

--- android/app/src/main/java/com/styleandglow/ai/MainActivity.java
+++ android/app/src/main/java/com/styleandglow/ai/MainActivity.java
@@ -1,5 +1,15 @@
 package com.styleandglow.ai;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Enable WebView debugging for chrome://inspect
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
+}
