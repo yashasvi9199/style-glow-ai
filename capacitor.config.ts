f1f8158c2f69f29ae339c03c3567a22950cbc2a8
Yash Haldiya <yashasvi9199@gmail.com>
2025-11-28 16:07:17 +0530

    fix: added server configs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.styleglow.ai',
   appName: 'Style Glow AI',
   webDir: 'dist',
+  server: {
+    cleartext: true,
+    hostname: 'localhost'
+  },
   plugins: {
     Android: {
       buildOptions: {
